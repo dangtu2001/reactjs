7d5afae50a0cf576f09309589fad3fca59f3e18d
Dang Tu <tu07122001@gmail.com>
2022-03-15 00:18:00 +0700

    15/3

--- src/containers/System/Admin/userRedux.js
+++ src/containers/System/Admin/userRedux.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
-import { getAllCodeService } from "../../../services/userService";
 import { LANGUAGES } from "../../../utils";
+import * as actions from "../../../store/actions";
+import './UserRedux.scss';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css'; // This only needs to be imported once in your app
 
 class userRedux extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            genderArr: []
+            genderArr: [],
+            positionArr: [],
+            roleArr: [],
+            previewImgUrl: '',
+            isOpen: false,
         }
     }
 
@@ -18,27 +25,70 @@ class userRedux extends Component {
     }
 
     async componentDidMount() {
-        try {
-            let res = await getAllCodeService('gender');
-            if (res && res.errCode === 0) {
-                this.setState({
-                    genderArr: res.data
-                })
-            }
-        } catch (e) {
-            console.log(e)
+        this.props.getGenderStart();
+        this.props.getPositionStart();
+        this.props.getRoleStart();
+        // try {
+        //     let res = await getAllCodeService('gender');
+        //     if (res && res.errCode === 0) {
+        //         this.setState({
+        //             genderArr: res.data
+        //         })
+        //     }
+        // } catch (e) {
+        //     console.log(e)
+        // }
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.genderRedux != this.props.genderRedux) {
+            this.setState({
+                genderArr: this.props.genderRedux
+            })
         }
+        if (prevProps.roleRedux != this.props.roleRedux) {
+            this.setState({
+                roleArr: this.props.roleRedux
+            })
+        }
+        if (prevProps.positionRedux != this.props.positionRedux) {
+            this.setState({
+                positionArr: this.props.positionRedux
+            })
+        }
+    }
+
+    handleOnchangeImage = (event) => {
+        let data = event.target.files;
+        let file = data[0];
+        if (file) {
+            let objectUrl = URL.createObjectURL(file);
+            this.setState({
+                previewImgUrl: objectUrl
+            })
+        }
+    }
+
+    openPreviewImage = () => {
+        if (!this.state.previewImgUrl) return;
+        this.setState({
+            isOpen: true
+        })
     }
 
 
     render() {
         let genders = this.state.genderArr;
+        let roles = this.state.roleArr;
+        let positions = this.state.positionArr;
         let language = this.props.language;
+        let isLoadingGender = this.props.isLoadingGender;
+        console.log("check props from redux:", this.state)
         return (
             <div className='user-redux-container'>
                 <div className='title'>
                     User Redux Tu
                 </div>
+                <div>{isLoadingGender === true ? 'Loading gender' : ''}</div>
                 <div className='user-redux-body' >
                     <div className='container'>
                         <div className='row'>
@@ -46,31 +96,31 @@ class userRedux extends Component {
                                 <FormattedMessage id="manage-user.add" />
                             </div>
                             <div className='col-3'>
-                                <label for=""><FormattedMessage id="manage-user.email" /></label>
+                                <label><FormattedMessage id="manage-user.email" /></label>
                                 <input type="email" className='form-control' />
                             </div>
                             <div className='col-3'>
-                                <label for=""><FormattedMessage id="manage-user.password" /></label>
+                                <label><FormattedMessage id="manage-user.password" /></label>
                                 <input type="password" className='form-control' />
                             </div>
                             <div className='col-3'>
-                                <label for=""><FormattedMessage id="manage-user.first-name" /></label>
+                                <label><FormattedMessage id="manage-user.first-name" /></label>
                                 <input type="text" className='form-control' />
                             </div>
                             <div className='col-3'>
-                                <label for=""><FormattedMessage id="manage-user.last-name" /></label>
+                                <label><FormattedMessage id="manage-user.last-name" /></label>
                                 <input type="text" className='form-control' />
                             </div>
                             <div className='col-3'>
-                                <label for=""><FormattedMessage id="manage-user.phone-number" /></label>
+                                <label><FormattedMessage id="manage-user.phone-number" /></label>
                                 <input type="text" className='form-control' />
                             </div>
                             <div className='col-9'>
-                                <label for=""><FormattedMessage id="manage-user.address" /></label>
+                                <label><FormattedMessage id="manage-user.address" /></label>
                                 <input type="text" className='form-control' />
                             </div>
                             <div className='col-3'>
-                                <label for=""><FormattedMessage id="manage-user.gender" /></label>
+                                <label><FormattedMessage id="manage-user.gender" /></label>
                                 <select className="form-control">
                                     {genders && genders.length > 0 &&
                                         genders.map((item, index) => {
@@ -86,22 +136,42 @@ class userRedux extends Component {
                                 </select>
                             </div>
                             <div className='col-3'>
-                                <label for=""><FormattedMessage id="manage-user.position" /></label>
+                                <label><FormattedMessage id="manage-user.position" /></label>
                                 <select className="form-control">
-                                    <option selected>Choose...</option>
-                                    <option>...</option>
+                                    {positions && positions.length > 0
+                                        && positions.map((item, index) => {
+                                            return (
+                                                <option key={index}>
+                                                    {language === LANGUAGES.VI ? item.valueVi : item.valueEn}
+                                                </option>
+                                            )
+                                        })}
                                 </select>
                             </div>
                             <div className='col-3'>
-                                <label for=""><FormattedMessage id="manage-user.role" /></label>
+                                <label><FormattedMessage id="manage-user.role" /></label>
                                 <select className="form-control">
-                                    <option selected>Choose...</option>
-                                    <option>...</option>
+                                    {roles && roles.length > 0
+                                        && roles.map((item, index) => {
+                                            return (
+                                                <option key={index}>
+                                                    {language === LANGUAGES.VI ? item.valueVi : item.valueEn}
+                                                </option>
+                                            )
+                                        })}
                                 </select>
                             </div>
                             <div className='col-3'>
-                                <label for=""><FormattedMessage id="manage-user.image" /></label>
-                                <input type="text" className='form-control' />
+                                <label><FormattedMessage id="manage-user.image" /></label>
+                                <div className='preview-img-container'>
+                                    <input id='previewImg' type="file" hidden
+                                        onChange={(event) => this.handleOnchangeImage(event)}
+                                    />
+                                    <label className='lable-upload' htmlFor='previewImg'>Tải ảnh <i className="fas fa-upload"></i></label>
+                                    <div className='preview-image' style={{ backgroundImage: `url(${this.state.previewImgUrl})` }}
+                                        onClick={() => this.openPreviewImage()}
+                                    ></div>
+                                </div>
                             </div>
                             <div className='col-12 mt-3'>
                                 <button className='btn btn-primary'><FormattedMessage id="manage-user.save" /></button>
@@ -109,6 +179,13 @@ class userRedux extends Component {
                         </div>
                     </div>
                 </div>
+                {this.state.isOpen === true &&
+                    <Lightbox
+                        mainSrc={this.state.previewImgUrl}
+                        onCloseRequest={() => this.setState({ isOpen: false })}
+
+                    />
+                }
             </div>
 
 
@@ -120,11 +197,18 @@ class userRedux extends Component {
 const mapStateToProps = state => {
     return {
         language: state.app.language,
+        genderRedux: state.admin.genders,
+        roleRedux: state.admin.roles,
+        positionRedux: state.admin.positions,
+        isLoadingGender: state.admin.isLoadingGender,
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
+        getGenderStart: () => dispatch(actions.fetchGenderStart()),
+        getPositionStart: () => dispatch(actions.fetchPositionStart()),
+        getRoleStart: () => dispatch(actions.fetchRoleStart()),
     };
 };
 
--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -1,33 +1,57 @@
 import actionTypes from '../actions/actionTypes';
 
 const initialState = {
-    isLoggedIn: false,
-    adminInfo: null
+    isLoadingGender: false,
+    genders: [],
+    roles: [],
+    positions: []
 }
 
-const appReducer = (state = initialState, action) => {
+const adminReducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.ADMIN_LOGIN_SUCCESS:
+        case actionTypes.FETCH_GENDER_START:
+            state.isLoadingGender = true;
+            // console.log("tu loading:", state);
             return {
-                ...state,
-                isLoggedIn: true,
-                adminInfo: action.adminInfo
+                ...state
             }
-        case actionTypes.ADMIN_LOGIN_FAIL:
+        case actionTypes.FETCH_GENDER_SUCCESS:
+            state.genders = action.data;
+            state.isLoadingGender = false;
+            // console.log("tu success:", state);
             return {
-                ...state,
-                isLoggedIn: false,
-                adminInfo: null
+                ...state
             }
-        case actionTypes.PROCESS_LOGOUT:
+        case actionTypes.FETCH_GENDER_FAILED:
+            state.isLoadingGender = false;
+            state.genders = [];
+            // console.log("tu failed:", action);
             return {
-                ...state,
-                isLoggedIn: false,
-                adminInfo: null
+                ...state
+            }
+        case actionTypes.FETCH_POSITION_SUCCESS:
+            state.positions = action.data;
+            return {
+                ...state
+            }
+        case actionTypes.FETCH_POSITION_FAILED:
+            state.positions = [];
+            return {
+                ...state
+            }
+        case actionTypes.FETCH_ROLE_SUCCESS:
+            state.roles = action.data;
+            return {
+                ...state
+            }
+        case actionTypes.FETCH_ROLE_FAILED:
+            state.roles = [];
+            return {
+                ...state
             }
         default:
             return state;
     }
 }
 
-export default appReducer;
+export default adminReducer;
